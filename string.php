251016259bb33f8ac553e253dda6de7fd2c86c09
Md Shah Alam <mdshahalam8283@gmail.com>
2024-05-17 23:38:41 +0600

    Module 10 Assignment 2

--- string.php
+++ string.php
@@ -4,7 +4,6 @@ $strings = ["Hello", "World", "PHP", "Programming"];
 
 foreach ($strings as $string) {
     $vowelCount = 0;
-    //$string = strtolower($string); // Convert string to lowercase to handle both upper and lower case vowels
     $strrev = strrev($string);
     for ($i = 0; $i < strlen($string); $i++) {
         if (strpos("aeiou", $string[$i]) !== false) {
